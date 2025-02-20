eacb1f1b3b743c39ff9bae2ab641183d6af31ae9
아서 <itsarthurlee@proton.me>
2025-02-20 15:25:23 +0900

    Added new task

--- lab5/row.py
+++ lab5/row.py
@@ -3,16 +3,13 @@ import re
 with open('row.txt', 'r', encoding='utf-8') as file:
     receipt_text = file.read()
 
-# Define regex patterns
 item_pattern = re.compile(r'(\d+)\.\n(.+?)\n(\d+,\d{3}) x (\d+,\d{2})\n(\d+,\d{2})', re.DOTALL)
 total_pattern = re.compile(r'ИТОГО:\n(\d+,\d{3},\d{2})')
 
-# Extract items
 items = item_pattern.findall(receipt_text)
 for item in items:
     print(f"Item {item[0]}: {item[1].strip()} - Quantity: {item[2]}, Unit Price: {item[3]}, Total: {item[4]}")
 
-# Extract total
 total = total_pattern.search(receipt_text)
 if total:
     print(f"Total: {total.group(1)}")
