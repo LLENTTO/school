c0bd3d8646b94c66c15199fbe257bfc4e849d252
아서 <itsarthurlee@proton.me>
2025-03-20 05:13:09 +0900

    fixed code

--- lab7/main/lab7_1.py
+++ lab7/main/lab7_1.py
@@ -1,25 +1,46 @@
 import pygame as pg
 import time
-
-curr = time.time()
-
-print(time.strftime("%H:%M:%S", time.localtime(curr)))
+import math
 
 pg.init()
+
 screen = pg.display.set_mode((1280, 720))
+pg.display.set_caption("Mickey Clock")
 running = True
 
-clock_img = pg.image.load("assets/clock.png")
+clock_img = pg.image.load("/assets/clock.png")
+minute_hand_img = pg.image.load("assets/rightarm.png")
+second_hand_img = pg.image.load("assets/leftarm.png")
+
+clock_center = (640, 360)  
+
+def rotate_image(image, angle, position):
+    rotated_image = pg.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center=position)
+    return rotated_image, new_rect
 
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
 
+    curr_time = time.localtime()
+    minutes = curr_time.tm_min
+    seconds = curr_time.tm_sec
+
+    minute_angle = -6 * minutes  
+    second_angle = -6 * seconds  
+
     screen.fill("white")
 
-    pg.display.flip()
-    screen.blit(clock_img, (500, 500))
+    screen.blit(clock_img, (440, 160))  
 
+    rotated_minute_hand, minute_hand_rect = rotate_image(minute_hand_img, minute_angle, clock_center)
+    screen.blit(rotated_minute_hand, minute_hand_rect)
+
+    rotated_second_hand, second_hand_rect = rotate_image(second_hand_img, second_angle, clock_center)
+    screen.blit(rotated_second_hand, second_hand_rect)
+
+    pg.display.flip()
 
 pg.quit()
--- lab7/main/lab7_3.py
+++ lab7/main/lab7_3.py
@@ -4,26 +4,50 @@ Draw circle - a red ball of size 50 x 50 (radius = 25) on white background.
  The ball should not leave the screen, i.e. user input that leads the
  ball to leave of the screen should be ignored
 """""
-
 import pygame
 
 pygame.init()
 
 WIDTH, HEIGHT = 1280, 720
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Moving Red Ball")
 
-WHITE = (255,255,255)
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+BALL_RADIUS = 25
+ball_x = WIDTH // 2  
+ball_y = HEIGHT // 2  
+MOVE_DISTANCE = 20
 
 activated = True
-
-while activated: 
+while activated:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             activated = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                new_y = ball_y - MOVE_DISTANCE
+                if new_y - BALL_RADIUS >= 0:  
+                    ball_y = new_y
+            elif event.key == pygame.K_DOWN:
+                new_y = ball_y + MOVE_DISTANCE
+                if new_y + BALL_RADIUS <= HEIGHT:  
+                    ball_y = new_y
+            elif event.key == pygame.K_LEFT:
+                new_x = ball_x - MOVE_DISTANCE
+                if new_x - BALL_RADIUS >= 0:  
+                    ball_x = new_x
+            elif event.key == pygame.K_RIGHT:
+                new_x = ball_x + MOVE_DISTANCE
+                if new_x + BALL_RADIUS <= WIDTH:  
+                    ball_x = new_x
 
     screen.fill(WHITE)
 
-    pygame.display.flip()
+    pygame.draw.circle(screen, RED, (ball_x, ball_y), BALL_RADIUS)
 
+    pygame.display.flip()
 
 pygame.quit()
